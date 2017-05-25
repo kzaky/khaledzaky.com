3ea6cabc7f0fbad7365048a35b982d9cc6e3ca18
Khaled Zaky <zakykhaled@gmail.com>
2017-05-24 20:27:31 -0400

    js edits
    Edits to javascript for filtering by year

--- _site/js/site.js
+++ _site/js/site.js
@@ -1,12 +1,10 @@
 $(function(){
+// Homepage | section.blog | article nav behavior
+  
+  // first, hide everything except 2017 posts be default
+  $(".article-item:not(.article-2017)").removeClass("show");
 
-  // Homepage | section.writing | article nav behavior
-  // NOTE: this code is mad ghetto and in no wise to be held up as a standard of JavaScript brilliance; so sorry bout that; but It Works™
-
-  // first, hide everything except 2015 posts be default
-  $(".article-item:not(.article-2016)").removeClass("show");
-
-  // if visitor clicks on All, show all posts
+  // if click on All, show all posts
   $("#articlesAllTrigger").on("click", function(event){
     event.preventDefault();
     $(".article-nav .year").removeClass("showing");
@@ -14,7 +12,7 @@ $(function(){
     $(".article-item").addClass("show");
   });
 
-  // if visitor clicks on 2017, show 2017 posts
+  // if click on 2017, show 2017 posts
   $("#articles2017Trigger").on("click", function(event){
     event.preventDefault();
     $(".article-nav .year").removeClass("showing");
@@ -23,7 +21,7 @@ $(function(){
     $(".article-item.article-2017").addClass("show");
   });
 
-  // if visitor clicks on 2016, show 2016 posts
+  // if click on 2016, show 2016 posts
   $("#articles2016Trigger").on("click", function(event){
     event.preventDefault();
     $(".article-nav .year").removeClass("showing");
