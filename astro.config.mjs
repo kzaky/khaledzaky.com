7c05baccf17bd69ea6f884a8aeb70f600fb49ad9
Khaled Zaky <kzaky@Khaleds-MacBook-Air.local>
2026-02-28 22:50:14 -0500

    feat: 404 handling for category pages, sitemap lastmod

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,12 @@ import rehypeLazyImages from './src/plugins/rehype-lazy-images.mjs';
 
 export default defineConfig({
   site: 'https://khaledzaky.com',
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [mdx(), sitemap({
+    serialize(item) {
+      // Remove trailing slash for consistency
+      return item;
+    },
+  }), tailwind()],
   output: 'static',
   markdown: {
     rehypePlugins: [rehypeLazyImages],
